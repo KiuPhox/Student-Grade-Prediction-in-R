2ff68b37720b83ac75c0c65b1786ec6f79f720b1
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-03-16 16:25:42 +0700

    Update xstk.r

--- xstk.r
+++ xstk.r
@@ -10,11 +10,8 @@ data<- read.csv(openExcel)
 new_DF <- data %>% select(G1, G2, G3, studytime, failures, absences, paid, sex)
 new_DF[is.na(new_DF)] = 6
 
-clean<-na.omit(clean)
-
-
 # Data vizualation
-G1_pl <- ggplot(new_DF, aes(x = G1)) + geom_bar(fill = "#69b3a2", color = "white") + labs(x = "Diem ky 1", y = "So diem") + ylim = c(0, 50) + xlim = c(0, 20)
-G2_pl <- ggplot(new_DF, aes(x = G2)) + geom_bar(fill = "#69b3a2", color = "white") + labs(x = "Diem ky 2", y = "So diem") + ylim = c(0, 50) + xlim = c(0, 20)
-G3_pl <- ggplot(new_DF, aes(x = G3)) + geom_bar(fill = "#69b3a2", color = "white") + labs(x = "Diem ky 3", y = "So diem") + ylim = c(0, 50) + xlim = c(0, 20)
+G1_pl <- ggplot2(new_DF, aes(x = G1)) + geom_bar(fill = "#69b3a2", color = "white") + labs(x = "Diem ky 1", y = "So diem") + ylim = c(0, 50) + xlim = c(0, 20)
+G2_pl <- ggplot2(new_DF, aes(x = G2)) + geom_bar(fill = "#69b3a2", color = "white") + labs(x = "Diem ky 2", y = "So diem") + ylim = c(0, 50) + xlim = c(0, 20)
+G3_pl <- ggplot2(new_DF, aes(x = G3)) + geom_bar(fill = "#69b3a2", color = "white") + labs(x = "Diem ky 3", y = "So diem") + ylim = c(0, 50) + xlim = c(0, 20)
 grid.arrange(G1_pl, G2_pl, G3_pl, ncol = 3)                                                                                                                        
