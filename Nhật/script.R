8449cfc2a98592408894b18d0cc1d0e5193a8270
Nguyễn Phan Anh Tuấn <79034091+KiuPhox@users.noreply.github.com>
2022-04-15 01:28:50 +0700

    Merge code, add new images

--- Nhật/script.R
+++ Nhật/script.R
@@ -1,3 +1,9 @@
+library("janitor")
+library("dplyr")
+library("ggplot2")
+library("gridExtra")
+library("GGally")
+library("performance")
 #1.  Import  data:  grade.csv
 openExcel <- file.choose()
 data<- read.csv(openExcel)
@@ -12,6 +18,8 @@ View(new_DF)
 apply(new_DF,2, function(new_DF) which(is.na(new_DF)))
 ### pp thay thế: xoá row có NA
 new_DF<- na.omit(new_DF)
+### Vì điểm 0 nhiều một cách bất thường nên ta xoá các hàng chứa điểm 0 để có mô hình hồi quy hợp lý
+new_DF <- new_DF %>% filter(G3 != 0) 
 View(new_DF)
 
 #3.  Data  visualization
@@ -25,8 +33,6 @@ max<- apply(new_DF[,c(1,2,3,6)], 2, max)
 ### results in tabular form
 descriptiveStatistics<- list(mean=mean,median=median,sd=sd,min=min,max=max) # Creating list of vectors
 descriptiveStatistics<- as.data.frame(descriptiveStatistics) # Converting list to data frame
-
-new_DF<- data[,c("G1","G2","G3","studytime","failures","absences","paid","sex")] #default 395 obs
 
 ## biến phân loại (studytime, failures, paid, sex)_lập bảng thống kê sl cho từng loại
 studytime <- as.data.frame(table(new_DF$studytime, dnn = list("Self-study time per week")), responseName = "Students")
@@ -46,3 +52,22 @@ boxplot(G3~sex, main= "boxplot of Sex considering G3",col = "green",border = "bl
 pairs(G3~G1,main= "Pairs of G1 considering G3",col = "red", pch = "8", data=new_DF)
 pairs(G3~G2,main= "Pairs of G2 considering G3",col = "red", pch = "8", data=new_DF)
 pairs(G3~absences,main= "Pairs of Absences considering G3",col = "red", pch = "8", data=new_DF)
+
+# Xây dựng các mô hình hồi quy tuyến tính
+new_DF <- new_DF %>% mutate_at(vars(,-G3,-absences),.funs=funs(factor)) %>% select(-c(G1,G2))
+M1 <- lm(formula = G3~.,data = new_DF)
+M2 <- lm(formula = G3~.-failures, data = new_DF)
+M3 <- lm(formula = G3~.-studytime, data = new_DF)
+M4 <- lm(formula = G3~.-paid, data = new_DF)
+compare_performance(M1, M2, M3, M4) # Vì M4 có giá trị AIC thấp nên ta chọn làm mô hình hồi quy tuyến tính
+
+plot(M4$fitted.values, M4$residuals,pch = 16,col = "black",xlab = "Fitted Values", ylab="Sai số",main = "Residual Plot")
+abline(h=0,col="red")
+
+# Dự báo điểm
+new_X <- new_DF %>% select(G3)
+new_X$G3_predicted <- round(predict(object = M4, newdata = new_DF), 2)
+
+x <- cbind(Dat = sum(new_X$G3 >= 10) / nrow(new_X),Ko_Dat = sum(new_X$G3 < 10) / nrow(new_X))
+y <- cbind(Dat = sum(new_X$G3_predicted >= 10) / nrow(new_X), Ko_Dat = sum(new_X$G3_predicted < 10) / nrow(new_X))
+rbind(x, y)
